697d2372e078ab24893c2670ec3a07b605cb8382
alderjanio <alderjanio@hotmail.com>
2019-02-14 21:14:00 -0400

    Banco de dados H2 e criação automatica de base de dados

--- src/main/java/com/alderjanio/cursomc/domain/Categoria.java
+++ src/main/java/com/alderjanio/cursomc/domain/Categoria.java
@@ -2,12 +2,24 @@ package com.alderjanio.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+
 public class Categoria implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
+	
+	
 	private Integer Id;
 	private String Nome;
 	
